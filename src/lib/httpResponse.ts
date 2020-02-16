2bfe00582ac76c1f83cae4f212e639357b373b3f
hector101 <hector1014really32@gmail.com>
2020-02-16 19:39:03 +0100

    add same site for cookies

--- src/lib/httpResponse.ts
+++ src/lib/httpResponse.ts
@@ -25,6 +25,7 @@ export function cookieResponse(res: Response, name: string, value: string, httpO
     httpOnly,
     secure: process.env.NODE_ENV === 'production',
     domain: process.env.DOMAIN,
+    sameSite: true,
   });
 }
 

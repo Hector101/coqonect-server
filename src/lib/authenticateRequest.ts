64f9a02e8443b7e7e607d73a01d329a3f8f265b0
Johnson Okoro <hector1014really32@gmail.com>
2020-06-07 09:03:47 +0100

    move to constants

--- src/lib/authenticateRequest.ts
+++ src/lib/authenticateRequest.ts
@@ -4,12 +4,15 @@ import IDecoded from '../interfaces/IDecoded';
 export default (decoded: IDecoded, req: IRequestWithAuthStatus) => {
   const platform = req.useragent && req.useragent.platform;
   const browser = req.useragent && req.useragent.browser;
+  const origin = 'coqonect.com';
+  const ADMIN_ROLE = 'admin';
+  const SUPER_ADMIN_ROLE = 'superAdmin';
 
   if (
     decoded.verified &&
     !decoded.blocked &&
-    decoded.iss === 'coqonect.com' &&
-    decoded.aud === 'coqonect.com' &&
+    decoded.iss === origin &&
+    decoded.aud === origin &&
     decoded.platform === platform &&
     decoded.browser === browser
   ) {
@@ -17,9 +20,9 @@ export default (decoded: IDecoded, req: IRequestWithAuthStatus) => {
     req.decoded = decoded;
 
     if (decoded.role) {
-      if (decoded.role === 'admin') {
+      if (decoded.role === ADMIN_ROLE) {
         req.isAdmin = true;
-      } else if (decoded.role === 'superAdmin') {
+      } else if (decoded.role === SUPER_ADMIN_ROLE) {
         req.isSuperAdmin = true;
       }
     }
